fd4f1d0dacd5ebe75dc73d5bfa3498172d89e371
Kyle Perkins <kyperkins@gmail.com>
2023-12-25 20:34:10 -0500

    add pieces and board

---
+++ app.js
@@ -0,0 +1,42 @@
+const gameBoard = document.querySelector("#gameboard");
+const player = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+
+const width = 8;
+
+const startPieces = [
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+    "","","","","","","","",
+    "","","","","","","","",
+    "","","","","","","","",
+    "","","","","","","","",
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+    rook, knight, bishop, queen, king, bishop, knight, rook
+];
+
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div')
+        square.classList.add('square');
+        square.innerHTML = startPiece;
+        square.setAttribute('square-id', i);
+        // square.classList.add('beige');
+        const row = Math.floor((63-i)/ 8) + 1;
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? 'beige' : "brown");
+        } else {
+            square.classList.add(i % 2 === 0 ? 'brown' : "beige");
+        }
+
+        if (i <= 15) {
+            square.firstChild.firstChild.classList.add('black');
+        } else if (i >= 48) {
+            square.firstChild.firstChild.classList.add('white');
+        }
+
+        gameBoard.append(square);
+    });
+}
+
+createBoard();
